9b012c172ce01d3737dcc66f8dab515c57372f2c
Carmichael Ferreira Pereira <carmichaelferreira06@gmail.com>
2019-02-08 20:48:27 -0200

    add: Placing the Markers.

--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react';
 
 const mapStyles = {
   width: '100%',
@@ -7,6 +7,41 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    };
+  }
+
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+  createMarkers(markers){
+    try{
+      console.log("Markers", markers);
+      return markers.map((element) =>{
+        return <Marker
+                onClick={this.onMarkerClick}
+                key = {element.id}
+                title={element.name}
+                name={element.name}
+                position={{lat: element.location.lat, lng: element.location.lng}}>
+        </Marker>
+      })
+    }
+    catch(error){
+      console.log("ERROR:",error);
+    }
+  }
+
   render() {
     return (
       <Map
@@ -14,10 +49,18 @@ export class MapContainer extends Component {
         zoom={14}
         style={mapStyles}
         initialCenter={{
-         lat: 40.7,
-         lng: -74
-        }}
-      />
+         lat: 40.752557,
+         lng: -73.973435
+        }}>
+        {this.createMarkers(this.props.markers)}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -11,7 +11,7 @@ class SideBar extends Component {
       })
     }
     catch(error){
-      console.log(error);
+      console.log("ERROR:", error);
     }
   }
 
