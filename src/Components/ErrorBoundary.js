0c9c406f71a67530eab817d35c93a52fc5786516
Carmichael Ferreira Pereira <carmichaelferreira06@gmail.com>
2019-02-17 22:32:29 -0300

    bug: Fixing an ErrorBoundary bug.

--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -7,18 +7,16 @@ class ErrorBoundary extends Component {
   }
 
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
   componentDidCatch(error, info) {
-    // You can also log the error to an error reporting service
-    alert(error, info);
+    console.log(error, info);
   }
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
+      return <h1 id="error-h1">Sorry, something went wrong with our Map :'(</h1>;
     }
 
     return this.props.children; 
