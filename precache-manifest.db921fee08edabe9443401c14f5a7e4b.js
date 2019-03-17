7b046e310781a46668cb3291943dbf9a537bc9b4
Carmichael Ferreira Pereira <carmichaelferreira06@gmail.com>
2019-03-17 13:57:37 -0300

    Updates

---
+++ precache-manifest.db921fee08edabe9443401c14f5a7e4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdd335d825c9871c0c12eba6538f27d2",
+    "url": "/neighborhood-map/static/media/powered-by-foursquare-grey.cdd335d8.png"
+  },
+  {
+    "revision": "f1627007f1dec8dd542c",
+    "url": "/neighborhood-map/static/js/runtime~main.f1627007.js"
+  },
+  {
+    "revision": "e3f2b655aeae8bc7ee61",
+    "url": "/neighborhood-map/static/js/main.e3f2b655.chunk.js"
+  },
+  {
+    "revision": "38a1f99d868b1afa4c2e",
+    "url": "/neighborhood-map/static/js/1.38a1f99d.chunk.js"
+  },
+  {
+    "revision": "e3f2b655aeae8bc7ee61",
+    "url": "/neighborhood-map/static/css/main.50dabc7a.chunk.css"
+  },
+  {
+    "revision": "12686527f7321bc63a20d7800d3d9cac",
+    "url": "/neighborhood-map/index.html"
+  }
+];
